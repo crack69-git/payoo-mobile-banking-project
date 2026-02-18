a605ba298c1dce79ec081ca28525befce7f57686
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-02-18 11:16:28 +0600

    more refined js using mechine function

--- script/cashout.js
+++ script/cashout.js
@@ -3,9 +3,8 @@ document.getElementById("cashout-btn").addEventListener("click", function () {
     // console.log("Cashout Number:", cashoutNumber);
     const cashoutAmount = getValueFromInput("cashout-amount");
     // console.log("Cashout Amount:", cashoutAmount);
-    const balanceElement = document.getElementById("balance");
-    const balance = balanceElement.innerText;
-    const newBalance = Number(balance) - Number(cashoutAmount);
+    const balanceElement = getBalance();
+    const newBalance = balanceElement - Number(cashoutAmount);
     console.log("cashout amount:", cashoutAmount);
     console.log("New Balance:", newBalance);
     if (newBalance < 0) {
@@ -15,11 +14,10 @@ document.getElementById("cashout-btn").addEventListener("click", function () {
     const pin = getValueFromInput("cashout-pin");
     if (pin === '1234') {
         alert("Cashout Successful");
-        balanceElement.innerText = newBalance;
+        setBalance(newBalance);
     }
     else {
         alert("Invalid Pin");
         return;
     }
-
 });
--- script/mechine.js
+++ script/mechine.js
@@ -2,5 +2,14 @@ function getValueFromInput(id) {
     const input = document.getElementById(id);
     const value = input.value;
     return value;
-
+}
+function getBalance() {
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log("Current Balance:", balance);
+    return Number(balance);
+}
+function setBalance(newBalance) {
+    const balanceElement = document.getElementById("balance");
+    balanceElement.innerText = newBalance;
 }
